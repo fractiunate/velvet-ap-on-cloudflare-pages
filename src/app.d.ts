bb138ce8e7f45fbc19dad2a8a90992a7dbe5947e
GITLAB_CI <ci@gitlab.com>
2022-07-28 16:42:03 +0200

    add @sveltejs/adapter-cloudflare

--- src/app.d.ts
+++ src/app.d.ts
@@ -8,6 +8,16 @@ declare namespace App {
 		userid: string;
 	}
 
+	interface Platform {
+		       env: {
+		           COUNTER: DurableObjectNamespace;
+		       };
+		       context: {
+		           waitUntil(promise: Promise<any>): void;
+		       };
+		       caches: CacheStorage & { default: Cache }
+		   }
+
 	// interface Platform {}
 
 	// interface Session {}
